948502eca161d825bf734e46edde384a9f19cdcc
reza <reza.sanjari@avl.com>
2024-08-20 11:27:04 +0200

    added todo comments, todo search in telescope is fixed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,13 +12,6 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
     telescope.setup({
       defaults = {
         path_display = { "smart" },
@@ -26,7 +19,6 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
           },
         },
       },
@@ -41,6 +33,8 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+	keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
+-- HACK: sdfdf
+

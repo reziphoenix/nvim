bd124835b38aad9c7c2cd0c37b2ff7e8befb1776
reza <reza.sanjari@avl.com>
2024-08-20 11:03:15 +0200

    added jk keymap to exit insert mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,3 +2,5 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", {desc = "move between buffers"})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {desc = "move between buffers"})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {desc = "move between buffers"})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {desc = "move between buffers"})
+
+vim.keymap.set("i", "jk", "<ESC>", {desc = "escape keymap"})

a773dcb1baa2c9bea251c2fdbf7ce8be13d1d097
reza <reza.sanjari@avl.com>
2024-08-20 09:27:48 +0200

    added buffer switch keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,4 @@
-return {
-["<C-h>"] = "<C-w>h",
-["<C-j>"] = "<C-w>j",
-["<C-k>"] = "<C-w>k",
-["<C-l>"] = "<C-w>l",
-}
+vim.keymap.set("n", "<C-h>", "<C-w>h", {desc = "move between buffers"})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {desc = "move between buffers"})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {desc = "move between buffers"})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {desc = "move between buffers"})

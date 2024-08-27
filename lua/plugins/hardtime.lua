8544d5e1a6597203ddd2f3495d11b7dcc7b6acf2
reza <reza.sanjari@avl.com>
2024-08-27 13:23:29 +0200

    added hardtime plugin

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return{
+   "m4xshen/hardtime.nvim",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {}
+}
+
+-- :Hardtime enable enable hardtime.nvim
+-- :Hardtime disable disable hardtime.nvim
+-- :Hardtime toggle toggle hardtime.nvim

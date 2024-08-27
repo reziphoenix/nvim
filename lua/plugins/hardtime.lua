f685a4fc5ce111aa2cd5b1d5748f612e5b4fcd16
reza <reza.sanjari@avl.com>
2024-08-27 13:44:33 +0200

    deleted hardtime :)

--- lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return{
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = {}
-}
-
--- :Hardtime enable enable hardtime.nvim
--- :Hardtime disable disable hardtime.nvim
--- :Hardtime toggle toggle hardtime.nvim

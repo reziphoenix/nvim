09108a3c0c9e9e94812f953e4615d10584655680
reza <reza.sanjari@avl.com>
2025-02-05 08:15:22 +0100

    fix: removed vscode theme from statusline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,7 +5,7 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'vscode',
+               -- theme = 'vscode',
                 component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
                 disabled_filetypes = {

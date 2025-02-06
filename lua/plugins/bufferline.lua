4eb8dce05266e59bdb78e90db8b9d80e2ef8c46f
reza <reza.sanjari@avl.com>
2025-02-06 11:41:52 +0100

    added buffers + nvim tree changes for buffers

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    config = function()
+      local bufferline = require("bufferline")
+
+      bufferline.setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          diagnostics_update_on_insert = true,
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            return "(" .. count .. ")"
+          end,
+          show_buffer_icons = true, -- Show filetype icons for buffers
+          indicator = {
+            style = "underline",
+          },
+        },
+      })
+
+      -- Keymaps
+	local opts = { noremap = true, silent = true }
+	vim.keymap.set("n", "<leader>bj", "<cmd>BufferLinePick<cr>", opts) -- Jump to a buffer
+	vim.keymap.set("n", "<leader>bf", "<cmd>Telescope buffers previewer=false<cr>", opts) -- Find buffer
+	vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<cr>", opts) -- Previous buffer
+	vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<cr>", opts) -- Next buffer
+	vim.keymap.set("n", "<leader>BW", "<cmd>noautocmd w<cr>", opts) -- Save without formatting (noautocmd)
+	vim.keymap.set("n", "<leader>be", "<cmd>BufferLinePickClose<cr>", opts) -- Pick buffer to close
+	vim.keymap.set("n", "<leader>bh", "<cmd>BufferLineCloseLeft<cr>", opts) -- Close all buffers to the left
+	vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseRight<cr>", opts) -- Close all buffers to the right
+	vim.keymap.set("n", "<leader>bD", "<cmd>BufferLineSortByDirectory<cr>", opts) -- Sort buffers by directory
+	vim.keymap.set("n", "<leader>bL", "<cmd>BufferLineSortByExtension<cr>", opts) -- Sort buffers by file type (language)
+    end,
+  },
+}
+
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,18 +1,40 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	config = function()
-		require("nvim-tree").setup({
-		  view = {
-		    width = 30,
-		  },
-		  renderer = {
-		    group_empty = true,
-		  },
-		})
-	
-		-- set keymaps
-    local keymap = vim.keymap -- for conciseness
+  "nvim-tree/nvim-tree.lua",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+          window_picker = {
+            enable = false, -- Ensures files open in the correct tab instead of a split
+          },
+        },
+      },
+    })
 
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-	end,
+    -- Automatically close NvimTree when a file is opened
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        if vim.bo.filetype == "NvimTree" then
+          vim.cmd("setlocal bufhidden=wipe")
+        end
+      end,
+    })
+
+    -- Set keymaps
+    local keymap = vim.keymap -- for conciseness
+    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- Toggle file explorer
+
+    -- Open files in a new tab when selected from NvimTree
+    keymap.set("n", "<CR>", ":lua require('nvim-tree').on_keypress('edit')<CR>:tabedit %<CR>", { noremap = true, silent = true })
+  end,
 }
+

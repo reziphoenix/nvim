5bf4664ecc985a4cd4a9e6cbcf5865f8b13c418c
reza <reza.sanjari@avl.com>
2025-02-07 10:47:58 +0100

    added quit and save hotkeys

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,21 +1,26 @@
-return{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-	  defaults = {
-	    ["<leader>e"] = { name = "+test" },
-	  },
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		icons = {
+			mappings = false,
+		},
+		defaults = {
+			["<leader>e"] = { name = "+test" },
+		},
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+		mode = { "n", "v" }, -- NORMAL and VISUAL mode
+		{ "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+		{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
+	},
 }
